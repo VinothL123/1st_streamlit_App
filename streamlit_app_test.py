e569349c9b1f715c068ba8b6afa815ed93a4877f
VinothL123 <150883370+VinothL123@users.noreply.github.com>
2023-12-06 11:22:41 +0530

    Update streamlit_app_test.py

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -14,5 +14,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 
 my_fruit_list1 = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list1.index),['Avacado'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list1.index),['Avocado'])
 streamlit.dataframe(my_fruit_list1)
