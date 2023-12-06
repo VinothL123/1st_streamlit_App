fe7c3996f05b0f3cda85ee3e31216c7fb3491a7f
VinothL123 <150883370+VinothL123@users.noreply.github.com>
2023-12-06 11:34:57 +0530

    Update streamlit_app_test.py

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -11,12 +11,12 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
 # my_fruit_list1 = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list1.index),['Avocado'])
 # streamlit.dataframe(my_fruit_list1)
 
-fruit_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado'])
-fruit_to_show=my_fruit_list.loc[fruit_selected]
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado'])
+fruit_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruit_to_show)
