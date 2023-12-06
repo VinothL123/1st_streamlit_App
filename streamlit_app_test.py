627016a54f7ef8d15f6c11474b8665d5b0e0fe4a
VinothL123 <150883370+VinothL123@users.noreply.github.com>
2023-12-06 10:45:37 +0530

    Update streamlit_app_test.py
    streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -4,6 +4,7 @@ streamlit.header('Sports')
 streamlit.text(('footbal,cricket....'))
 
 streamlit.header('Breakfast Menu - 🥣 🥗 🐔 🥑🍞')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
